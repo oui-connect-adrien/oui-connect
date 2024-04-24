81700369eeadba872960fe20fa2c8599aab87b56
Adrien <adrien.poirier@oui-connect.fr>
2024-04-24 15:03:29 +0200

    Update page.tsx

--- app/(public)/(routes)/tarifs/page.tsx
+++ app/(public)/(routes)/tarifs/page.tsx
@@ -36,21 +36,23 @@ const TarifsPage = () => {
 			className="min-h-screen py-4 antialiased overflow-hidden"
 		>
 			<div className="pb-20 pt-14 px-4 flex flex-col items-center">
-				<div className="mb-14 border-b lg:container border-4">
+				<div className="mb-14 shadow-b-2 lg:container">
 					<h1 className="text-3xl md:text-6xl font-bold mb-8 mt-20 relative text-center text-zinc-700 max-w-6xl mx-auto">
 						Formules d'abonnement
 					</h1>
-					<h2 className="font-regular text-base md:text-xl text-zinc-500 tracking-wide mb-8 text-center max-w-3xl mx-auto antialiased">
+					<h2 className="font-regular text-base md:text-xl text-zinc-500 tracking-wide text-center max-w-3xl mx-auto antialiased">
 						Choisissez la formule qui vous convient le mieux
 					</h2>
 				</div>
-				<div className="[perspective:1000px] min-h-screen relative flex flex-col mx-auto w-full items-start justify-start">
-					<Tabs
-						contentClassName="bg-white border-4"
-						activeTabClassName="bg-gradient-to-r from-primary to-transparent"
-						tabClassName="bg-gradient-to-r from-transparent to-primary/10"
-						tabs={tabs}
-					/>
+				<div className="min-h-96 relative mx-auto w-full [mask-image:linear-gradient(to_top,transparent,white_20%,white_90%,transparent)] [webkit-mask-image:linear-gradient(to_top,transparent,white_20%,white_80%,transparent)] bg-grid-slate-100/70">
+					<div className="mt-14 grow flex flex-col [perspective:1000px] items-start justify-start">
+						<Tabs
+							contentClassName="bg-transparent border-4"
+							activeTabClassName="bg-gradient-to-r from-primary to-transparent"
+							tabClassName="bg-gradient-to-r from-transparent to-primary/10"
+							tabs={tabs}
+						/>
+					</div>
 				</div>
 			</div>
 		</section>
