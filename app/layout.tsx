969b23c762347bbf484b69fc0e54222160d101d0
Baptistews <b.warion@oui-connect.fr>
2024-06-24 16:55:24 +0200

    ...

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Toaster } from "sonner";
 import "./globals.css";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
